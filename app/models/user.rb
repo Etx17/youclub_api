16f229aa4a44e3a1da9f39633890159ad81d19b2
Etienne de Dianous <etiennededi@hotmail.fr>
2023-08-03 10:07:19 -0400

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_one :club
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+  enum role: { user: 0, club: 1, admin: 2 }
 end
