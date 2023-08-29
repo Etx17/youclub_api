bf422d5ba5ba07783f91f2c82020f1c36b7a434e
Etienne de Dianous <etiennededi@hotmail.fr>
2023-08-29 20:36:56 +0200

    commented migration

--- db/migrate/20230807085223_change_subgroups_column_type.rb
+++ db/migrate/20230807085223_change_subgroups_column_type.rb
@@ -1,7 +1,7 @@
 class ChangeSubgroupsColumnType < ActiveRecord::Migration[7.0]
   def change
     # change_column :sub_groups, :min_price, :integer, comment: 'Stored in cents'
-    change_column :sub_groups, :max_price, :integer, comment: 'Stored in cents'
+    # change_column :sub_groups, :max_price, :integer, comment: 'Stored in cents'
     change_column :sub_groups, :subscription_by_reccurence_price, :integer, comment: 'Stored in cents'
   end
 end
