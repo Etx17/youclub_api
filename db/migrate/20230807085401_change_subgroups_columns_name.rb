88d2709aa1b22da85b2886f30a1ef82dfb8d057e
Etienne de Dianous <etiennededi@hotmail.fr>
2023-10-23 15:06:30 +0200

    ok

--- db/migrate/20230807085401_change_subgroups_columns_name.rb
+++ db/migrate/20230807085401_change_subgroups_columns_name.rb
@@ -1,7 +1,7 @@
 class ChangeSubgroupsColumnsName < ActiveRecord::Migration[7.0]
   def change
-    # rename_column :sub_groups, :min_price, :min_price_cents
-    # rename_column :sub_groups, :max_price, :max_price_cents
-    # rename_column :sub_groups, :subscription_by_reccurence_price, :subscription_by_reccurence_price_cents
+    rename_column :sub_groups, :min_price, :min_price_cents
+    rename_column :sub_groups, :max_price, :max_price_cents
+    rename_column :sub_groups, :subscription_by_reccurence_price, :subscription_by_reccurence_price_cents
   end
 end
