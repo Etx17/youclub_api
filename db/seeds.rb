45220c72cb8773b6bab2460afed596ed5f0e5adc
Etienne de Dianous <etiennededi@hotmail.fr>
2023-11-03 13:35:15 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# p 'Clearing existing records...'
-# Club.destroy_all
-# Activity.destroy_all
-# SubGroup.destroy_all
-# Schedule.destroy_all
-# TimeSlot.destroy_all
-# User.destroy_all
+p 'Clearing existing records...'
+Club.destroy_all
+Activity.destroy_all
+SubGroup.destroy_all
+Schedule.destroy_all
+TimeSlot.destroy_all
+User.destroy_all
 
-# etienne = User.create!(
-#   email: 'etiennededianous@gmail.com',
-#   password: 'my_encrypted_password',
-#   role: 'user',
-#   sub_id: '24ddf6e7-9d4e-4339-9bf3-ea395e32b089'
-# )
+etienne = User.create!(
+  email: 'etiennededianous@gmail.com',
+  password: 'my_encrypted_password',
+  role: 'user',
+  sub_id: '24ddf6e7-9d4e-4339-9bf3-ea395e32b089'
+)
 
 # p "seeding all parisian sports clubs from api datagouv "
 # require_relative 'seeds/assos_sport_paris'
