076da28cc9860ec6a0228014f08d74607e46470e
Etienne de Dianous <etiennededi@hotmail.fr>
2023-11-11 10:41:11 +0100

    ok

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,8 +1,19 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: 'youclubstaging-42da65c4b5e7.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: Rails.application.credentials.dig(:email),
+    password: Rails.application.credentials.dig(:password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
