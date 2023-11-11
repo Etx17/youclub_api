123d1ca23635014afdd94774c2cc66b84e14f8a3
Etienne de Dianous <etiennededi@hotmail.fr>
2023-11-11 11:44:56 +0100

    wip

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,6 +3,7 @@ class ActivitiesController < ApplicationController
   end
 
   def new
+    @activity = Activity.new()
   end
 
   def show
@@ -14,11 +15,44 @@ class ActivitiesController < ApplicationController
   end
 
   def edit
+    @activity = Activity.find(params[:id])
   end
 
   def update
+    @activity = Activity.find(params[:id])
+    if @activity.update(activity_params)
+      redirect_to edit_activity_path(@activity), notice: 'Activity was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def delete
   end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(
+      :name,
+      :geo_point,
+      :category,
+      :address,
+      :actual_zipcode,
+      :free_trial,
+      :is_looking_for_volunteer,
+      :subcategories,
+      :recurrence,
+      :subscription_link,
+      :short_description,
+      :full_description,
+      :club_id,
+      :images, # Assuming `images` is an array of image data
+      :category_number,
+      :subcategory_number,
+      :phone_number,
+      :photos
+    )
+  end
+
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -27,4 +27,6 @@ class Activity < ApplicationRecord
   belongs_to :club
   has_many :sub_groups, dependent: :destroy
   scope :by_zipcode, -> (zipcode) { where(actual_zipcode: zipcode) }
+  has_many_attached :photos
+
 end
