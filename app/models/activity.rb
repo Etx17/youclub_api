3748926d36341579ce586d7e3b75a8aac0c8fa58
Etienne de Dianous <etiennededi@hotmail.fr>
2023-11-14 10:46:40 +0100

    ok

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -28,5 +28,23 @@ class Activity < ApplicationRecord
   has_many :sub_groups, dependent: :destroy
   scope :by_zipcode, -> (zipcode) { where(actual_zipcode: zipcode) }
   has_many_attached :photos
+  after_create :add_subcategory_to_club
+  before_destroy :remove_subcategory_from_club
 
+  # callback
+  def add_subcategory_to_club
+    unless club.subcategories.include?(self.subcategories)
+      new_subcategories = club.subcategories + [self.subcategories]
+      club.update(subcategories: new_subcategories)
+    end
+  end
+
+  def remove_subcategory_from_club
+    # Check if no other activities in the club have the same subcategory
+    if club.activities.where.not(id: self.id).where(subcategories: self.subcategories).none?
+      # If unique, update the club's subcategories
+      updated_subcategories = club.subcategories - [self.subcategories]
+      club.update(subcategories: updated_subcategories)
+    end
+  end
 end
