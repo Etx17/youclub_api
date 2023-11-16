4c56a6eac5d524f960f91eeffb3575feaaf4b0b8
Etienne de Dianous <etiennededi@hotmail.fr>
2023-11-16 11:12:54 +0100

    adde validation 1000 chars on comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :club
 
   validates :user_id, uniqueness: { scope: :club_id }
-  # validates :content, length: { maximum: 50 }
+  validates :content, length: { maximum: 1000 }
 
 end
