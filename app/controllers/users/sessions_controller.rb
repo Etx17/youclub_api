1e462309e7fa54224448f96de0712a4f2f0adca1
etienne de dianous <etiennededi@hotmail.fr>
2023-11-20 18:52:15 +0100

    fixed sign up rediretion fail

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,7 +20,8 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || clubs_path
+    # stored_location_for(resource_or_scope) ||
+    clubs_path
   end
   # protected
 
