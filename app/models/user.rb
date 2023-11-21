ddd8717dd78f1cd0f412344a47fe0ec028929425
Etienne de Dianous <etiennededi@hotmail.fr>
2023-11-21 13:53:11 +0100

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ApplicationRecord
   end
 
   def send_onboarding_mail
-    return if clubs.where(onboarding_mail_sent: true).any?
+    p 'sending onboarding mail...'
     postmark_client = Postmark::ApiClient.new(Rails.application.credentials.dig(:postmark_api_token))
     postmark_client.deliver_with_template(
       from: 'contact@youclub.fr',
